2db82127d39c4a2943a22fad8c7b1530aa00f837
John-Evan Karcenty <john-evan@live.fr>
2018-12-05 19:17:52 +0100

    improvements when display if done

--- frontend/src/components/TaskItem.js
+++ frontend/src/components/TaskItem.js
@@ -6,20 +6,26 @@ function TaskItem(props) {
     var myBeginning = new Date(beginning);
     const end = props.task.end;
     var myEnd = new Date(end);
-    const done = props.task.done? "done" : "pending";
     
+    //function pour montrer l'état de la tâche avec des couleurs différentes selon l'urgence 
     function renderDone() {
       const now = new Date();
-      const duration = props.task.end-now;
-      if ("done" === done){
-        return <td className="text-success">{done}</td>
-      } else if ("pending" === done && duration/3600000<120 ) {
-        return <td className="text-danger">{done}</td>
-      } else {
-        return <td className="text-warning">{done}</td>
+      const duration = myEnd-now;
+      console.log(duration/60000)
+      if (props.task.done && duration>0){ //si c'est fait
+        return <td className="text-success">done</td>
+      } else if (props.task.done && duration<=0) { // si c'est fait et c'est passé
+        return <td className=".text-muted">done</td>
+      } else if (!props.task.done && duration<0) { // si c'est à faire et c'est passé
+        return <td className=".text-muted">missed</td>
+      } else if (!props.task.done && duration/60000>120 ) { // si c'est à faire mais il y a le temps
+        return <td className="text-warning">pending </td>
+      } else { // si c'est à faire urgemment
+        return <td className="text-danger">pending in {Math.trunc(duration/60000)} min</td>
       }
     }
 
+    //affiche la tâche sous forme de ligne du tableau
     return (
       <tr className="Task">
         <td className="room">{props.task.room}</td>

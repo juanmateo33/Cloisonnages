75f950ccc5014b4f3ad1a3feaf6413a007983ea9
John-Evan Karcenty <john-evan@live.fr>
2018-12-13 00:23:31 +0100

    change the update function

--- frontend/src/components/Tasks.js
+++ frontend/src/components/Tasks.js
@@ -29,13 +29,13 @@ class Tasks extends Component {
   }
 
   //function updatebutton
-   handleClick(task, hasBeenDone){
+  handleClick(task, hasBeenDone){
     const body = { done: hasBeenDone};
     const url = `/tasks/${task._id}`;
     axios.put(url, body)
     .then(//une fois la base de donnée modifiée, on peut la recharger.
       //pour l'instant je n'arrive pas à changer uniquement un élément de la liste de Tasks directement sur le front
-      this.fetchTasks()
+      () => this.fetchTasks()
     ).catch(err=> {console.log("impossible de modifier la tâche");
     console.log(err)})
 }
